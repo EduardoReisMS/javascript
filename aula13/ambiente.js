4f19192e1975420d2f1429f831939ff4b71cb2cb
Eduardo Reis <124206607+EduardoReisMS@users.noreply.github.com>
2023-08-01 09:13:03 -0300

    exercicios

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var contador = 1
+while (contador <= 6) { // Enquanto a varíavel "contador" for menor ou igual a 6, ele fará a repetição
+     console.log(`Passo ${contador}`)
+     contador++ // é a mesma coisa que: contador = contador + 1. Ou seja, ele vai somar e parar quando chegar no 6 / c+1
+}
+// Aqui o teste lógico é feito no íniciovar contador = 1
+// While = Enquanto
+
+do { 
+    console.log(`Passo ${contador}`)
+    contador ++
+} while (contador <=6)
+// Faça tudo isso enquanto o contador for menor ou igual a 6
+// Do = Faça
---
+++ aula14/ambiente.js
@@ -0,0 +1,7 @@
+console.log('Vai começar...')
+for (var contador = 1; contador <= 10; contador++) {
+    console.log(`Passo ${contador}`)
+}
+console.log('FIM')
+// um while mais prático
+// for ( variavel (inicio); condição(teste_logico) ; incremento/soma até que a condição seja atingida )
